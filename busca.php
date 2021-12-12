82ad496c61e447ef1831bc28bf1c33b169b9bcd2
Lucas Lima <fstudies.lls@gmail.com>
2021-12-12 11:38:03 -0300

    barra-busca

---
+++ busca.php
@@ -0,0 +1,18 @@
+<?php
+    //incluindo a conexao com o banco de dados
+    include_once('conexao.php');
+    //recuperando o valor da palavra
+    $produtos = $_POST['palavra'];
+    //pesuisar no banco de dados o nome do produto pelo campo nome;
+    $produtos = "SELECT * FROM  listarEstoque WERE nome LIKE '$%produtos%'";
+    $resultado_buscas = mysqli_query($conn, $produtos);
+    if(mysqli_num_rows($resultado_buscas) <= 0){
+        echo "Nenhum item encontrado";
+    } else {
+        whilw(rows = mysqli_fetch_assoc($resultado_buscas)){
+            echo "<li>".$rows['nome']."</li>"
+        }
+        
+    }
+
+?>
